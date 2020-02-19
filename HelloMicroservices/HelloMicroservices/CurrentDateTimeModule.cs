6f48480baffde065b0b9ff2e779e6696ff7a74fd
LAN\VMaryan <Vitaly.Marian@global-e.com>
2020-02-19 18:11:23 +0200

    Added "HelloMicroservices" project

---
+++ HelloMicroservices/HelloMicroservices/CurrentDateTimeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace HelloMicroservices
+{
+    public class CurrentDateTimeModule : NancyModule
+    {
+        public CurrentDateTimeModule()
+        {
+            Get("/", handler => DateTime.UtcNow ); // Define route and routes handler.
+        }
+    }
+}
